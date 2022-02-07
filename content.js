86d7d0968d2855796a5d4ee8674d9a0e2c3b93cd
Jonathan Sivahera <jsivahera@icloud.com>
2022-02-07 17:26:20 +0200

    Fix add function

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 const bookList = document.querySelector('.book_list');
-const removeBtn = document.querySelectorAll('.remove_book_btn');
 
 const books = [
   {
@@ -13,15 +12,26 @@ const books = [
 ];
 
 const displayBooks = () => {
+	bookList.innerHTML = '';
   books.forEach((book) => {
-    bookList.innerHTML += `
-      <article class="book">
-        <p class="book_title">${book.title}</p>
-        <p class="book_author">${book.author}</p>
-        <button class="remove_book_btn" type="button">Remove</button>
-        <hr />
-      </article>
-    `;
+ 	const li = document.createElement('li');
+	const article = document.createElement('article');
+	const title = document.createElement('p');
+	const author = document.createElement('p');
+	const button = document.createElement('button');
+	
+	
+	title.className = "book_title";
+	title.textContent = book.title;
+	author.className = "book_author";
+	author.textContent = book.author;
+	button.className = "remove_book_btn";
+	button.textContent = "Remove";
+	article.append(title, author, button);
+	li.className = "book";
+	li.appendChild(article);
+	
+  bookList.appendChild(li);
   });
 };
 
@@ -36,12 +46,14 @@ const addBook = (title, author) => {
 
 const removeBook = (index) => {
   books.splice(index, 1);
+  displayBooks();
 };
 
-document.addEventListener('DOMContentLoaded', displayBooks);
+document.addEventListener('load', displayBooks);
 
-// removeBtn.forEach((btn, index) => {
-//   console.log(index);
-//   console.log(btn);
-//   console.log('test');
-// });
+const removeBtn = document.querySelectorAll('.remove_book_btn');
+removeBtn.forEach((btn, index) => {
+   btn.addEventListener('click', (event) => {
+     removeBook(index);
+   });
+});
