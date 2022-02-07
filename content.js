f557379a1845f6710abdf07639dea7fb071aaea8
Jonathan Sivahera <jsivahera@icloud.com>
2022-02-07 16:09:57 +0200

    Autofix linter errors

--- content.js
+++ content.js
@@ -1,27 +1,27 @@
 const bookList = document.querySelector('.book_list');
 
 const books = [
-{
-author: 'ipsum',
-title: 'Testyy'
-},
-{
-author: 'lorem',
-title: 'Testeroo'
-}
+  {
+    author: 'ipsum',
+    title: 'Testyy',
+  },
+  {
+    author: 'lorem',
+    title: 'Testeroo',
+  },
 ];
 
 function displayBooks() {
-	books.forEach((book) => {
-		bookList.innerHTML += `
+  books.forEach((book) => {
+    bookList.innerHTML += `
 		<article class="book">
         <p class="book_title">${book.title}</p>
         <p class="book_author">${book.author}</p>
         <button class="remove_book_btn" type="button">Remove</button>
         <hr />
     </article>
-		`
-	});
+		`;
+  });
 }
 
 document.addEventListener('DOMContentLoaded', displayBooks);
