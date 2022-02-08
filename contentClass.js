f37bdb724886f72f7219bab89c9d07e2f66f3b5b
Jonathan Sivahera <jsivahera@icloud.com>
2022-02-08 14:07:27 +0200

    Improve the content generator function

--- contentClass.js
+++ contentClass.js
@@ -33,18 +33,15 @@ class Book {
     this.books.forEach((book, index) => {
       const li = document.createElement('li');
       const article = document.createElement('article');
-      const title = document.createElement('p');
-      const author = document.createElement('p');
+      const description = document.createElement('p');
       const button = document.createElement('button');
 
-      title.className = 'book_title';
-      title.textContent = book.title;
-      author.className = 'book_author';
-      author.textContent = book.author;
+      description.className = 'book_info';
+      description.textContent = `"${book.title}" by ${book.author}`
       button.className = 'remove_book_btn';
       button.textContent = 'Remove';
 
-      article.append(title, author, button);
+      article.append(description, button);
       li.className = 'book';
       li.appendChild(article);
 
