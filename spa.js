d4aa1dba36f24e778dae07c076d63e40f111b423
Jonathan Sivahera <jsivahera@icloud.com>
2022-02-09 14:44:14 +0200

    Fix linters

--- spa.js
+++ spa.js
@@ -1,7 +1,6 @@
 const bookListSection = document.querySelector('.book_list_section');
 const AddbookSection = document.querySelector('.add_book');
 const contactSection = document.querySelector('.contact_section');
-const header = document.querySelector('.nav_header');
 const timeDisplay = document.querySelector('.time-display');
 
 const listBtn = document.querySelector('.list');
@@ -9,28 +8,28 @@ const addNewBtn = document.querySelector('.add_new');
 const contactBtn = document.querySelector('.contact');
 
 const setTime = () => {
-  let date = new Date();
+  const date = new Date();
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  let month = months[date.getMonth()];
-  let year = date.getFullYear();
-  let day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  const day = date.getDate();
   let minutes = date.getMinutes();
-  let hours = date.getHours();
+  const hours = date.getHours();
   let seconds = date.getSeconds();
 
-  if(seconds.toString().length < 2) {
-    seconds = '0' + seconds;
+  if (seconds.toString().length < 2) {
+    seconds = `0${seconds}`;
   }
-  if(minutes.toString().length < 2) {
-    minutes = '0' + minutes;
+  if (minutes.toString().length < 2) {
+    minutes = `0${minutes}`;
   }
 
-  let time = `${month} ${day}th ${year}, ${hours}:${minutes}:${seconds}`;
+  const time = `${month} ${day}th ${year}, ${hours}:${minutes}:${seconds}`;
   timeDisplay.textContent = '';
   timeDisplay.textContent = `${time}`;
-}
+};
 
-// setInterval(setTime, 1000);
+setInterval(setTime, 1000);
 
 listBtn.addEventListener('click', () => {
   bookListSection.style.display = 'flex';
